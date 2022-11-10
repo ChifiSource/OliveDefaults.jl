867b663baa6820a853640af60739c53842162627
emmy <52672675+emmettgb@users.noreply.github.com>
2022-11-10 04:15:14 -0600

    init ! :D

---
+++ src/OliveDefaults.jl
@@ -0,0 +1,19 @@
+module OliveDefaults
+
+function dark_mode(c::Connection)
+    darkicon = topbar_icon("darkico", "dark_mode")
+    on(c, darkicon, "click") do cm::ComponentModifier
+        if cm["olivestyle"]["dark"] == "false"
+            set_text!(cm, darkicon, "light_mode")
+            set_children!(cm, "olivestyle", olivesheetdark()[:children])
+            cm["olivestyle"] = "dark" => "true"
+        else
+            set_text!(cm, darkicon, "dark_mode")
+            set_children!(cm, "olivestyle", olivesheet()[:children])
+            cm["olivestyle"] = "dark" => "false"
+        end
+    end
+    darkicon::Component{:span}
+end
+
+end # module
